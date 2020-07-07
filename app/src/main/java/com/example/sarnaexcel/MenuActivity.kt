c25276c666271a1f618d41ab8df0116c9ba5adeb
Piotr Sarna <piotr.m.sarna@gmail.com>
2020-07-07 10:25:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sarnaexcel/MenuActivity.kt
@@ -0,0 +1,16 @@
+package com.example.sarnaexcel
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_menu)
+    }
+
+
+}
